ebfd27c82fd4f6e1f6e1473f4a40c815db8c0aba
PGaillot <pr.gaillot@gmail.com>
2024-02-08 01:35:50 +0100

    ajuste design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,71 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TransparentBenefitCardComponent } from './components/transparent-benefits-card/transparent-benefits-card.component';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { TransparentBenefitCardComponent } from './components/transparent-benefits-card/transparent-benefits-card.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [TransparentBenefitCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'rust-handler';
+  title = 'rust-handler'
+  mouseX = 0
+  mouseY = 0
+
+  benefitCards: TransparentBenefitCardComponent[] = [
+    {
+      title: 'Advanced custom',
+      description:
+        'Have full control over server configuration, crafting a unique gaming experience.',
+      imgUrl: 'https://files.facepunch.com/rust/item/axe.salvaged_512.png',
+      top: '-2rem',
+      right: null,
+      bottom: null,
+      left: '-2rem',
+      // onMouseEnter: (event) => {},
+    },
+    {
+      title: 'Automatic and reliable backups',
+      description:
+        'Ensure data safety with automated backups and optional FTP/SFTP transfer',
+      imgUrl: 'https://files.facepunch.com/rust/item/icepick.salvaged_512.png',
+      top: '-2rem',
+      right: '-2rem',
+      bottom: null,
+      left: null,
+      // onMouseEnter: (event) => {},
+    },
+    {
+      title: 'Enhanced security',
+      description:
+        'Protect your server from potential errors with built-in security measures.',
+      imgUrl: 'https://files.facepunch.com/rust/item/grenade.beancan_512.png',
+      top: null,
+      right: null,
+      bottom: '-6rem',
+      left: '30%',
+      // onMouseEnter:(event) => {},
+    },
+  ]
+
+  @ViewChild('blurGradient', { static: true }) blurGradientRef!: ElementRef
+  // @HostListener('document:mousemove', ['$event'])
+  // onMouseMove(event: MouseEvent) {
+  //   const pageWidth = document.documentElement.clientWidth
+  //   const pageHeight = document.documentElement.clientHeight
+  //   this.mouseX = event.clientX
+  //   this.mouseY = event.clientY
+  //   const leftPosition: string = `${(this.mouseX * 100) / pageWidth}%`
+  //   const topPosition: string = `${(this.mouseY * 100) / pageHeight}%`
+  //   console.log(leftPosition)
+  //   this.blurGradientRef.nativeElement.style.left = leftPosition // Changer la valeur selon vos besoins
+  //   this.blurGradientRef.nativeElement.style.top = topPosition // Changer la valeur selon vos besoins
+  // }
+
+  ngOnInit(): void {
+    this.blurGradientRef.nativeElement.style.left = '50%' // Changer la valeur selon vos besoins
+    this.blurGradientRef.nativeElement.style.top = '50%' // Changer la valeur selon vos besoins
+  }
 }
--- src/app/components/transparent-benefits-card/transparent-benefits-card.component.ts
+++ src/app/components/transparent-benefits-card/transparent-benefits-card.component.ts
@@ -1,16 +1,63 @@
-import { Component, Input } from "@angular/core";
+import { NgStyle } from '@angular/common'
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Input,
+  Renderer2,
+} from '@angular/core'
+
+export interface TransparentBenefitCardComponentData {
+  title: string
+  description: string
+  imgUrl: string
+  top: string | null
+  right: string | null
+  bottom: string | null
+  left: string | null
+}
 
 @Component({
-    selector:'transparent-benefits-card',
-    standalone:true,
-    templateUrl:'./transparent-benefits-card.component.html',
-    styleUrls:['./transparent-benefits-card.component.scss']
+  selector: 'transparent-benefits-card',
+  standalone: true,
+  imports: [NgStyle],
+  styleUrls: ['./transparent-benefits-card.component.scss'],
+  template: `
+    <div
+      class="transparent-benefits-card-container"
+      [ngStyle]="{ top: top, right: right, bottom: bottom, left: left }"
+    >
+      <div class="transparent-container">
+        <img
+          class="transparent-container-img"
+          [src]="imgUrl"
+          alt=""
+          srcset=""
+        />
+        <div class="transparent-container-title">{{ title }}</div>
+        <div>{{ description }}</div>
+      </div>
+    </div>
+  `,
 })
+export class TransparentBenefitCardComponent {
+  @Input() title!: string
+  @Input() description!: string
+  @Input() imgUrl!: string
 
-export class TransparentBenefitCardComponent{
+  @Input() top: string | null = null
+  @Input() right: string | null = null
+  @Input() bottom: string | null = null
+  @Input() left: string | null = null
 
-    @Input() title!:string;
-    @Input() description!:string;
-    @Input() imgUrl!:string;
-
+//   @HostListener('mouseenter', ['$event'])
+//   onMouseEnter(event: MouseEvent) {
+//     const element = event.target as HTMLElement
+//     const container = element.getElementsByClassName(
+//       'transparent-container',
+//     )[0] as HTMLElement
+//     const reflect = document.createElement('div') as HTMLElement;
+//     reflect.classList.add('reflect-card');
+//     container.appendChild(reflect)
+//   }
 }
