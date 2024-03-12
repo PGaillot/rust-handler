d13e150e2cb785424a68f7d8536f81b4bc4069f9
PGaillot <pr.gaillot@gmail.com>
2024-03-12 20:00:29 +0100

    change dynamic title behavior.

--- src/app/components/dynamic-title/dynamic-title.component.ts
+++ src/app/components/dynamic-title/dynamic-title.component.ts
@@ -6,6 +6,7 @@ import {
   HostListener,
   Input,
   OnInit,
+  Renderer2,
   ViewChild,
   input,
 } from '@angular/core'
@@ -16,7 +17,7 @@ import {
   imports: [NgClass],
   styleUrl: './dynamic-title.component.scss',
   template: `
-    <div id="title-container">
+    <div #titleCont id="title-container">
       @for(char of charArray; track char ; let i = $index){ @if(char !== ' '){
       <div class="char-container">
         <div class="char-wrapper" [ngClass]="'cw-' + i">
@@ -31,57 +32,64 @@ import {
     </div>
   `,
 })
-export class DynamicTitleComponent implements OnInit, AfterViewInit{
-    @Input() title!: string
-    charArray: String[] = []
+export class DynamicTitleComponent implements OnInit, AfterViewInit {
+  @Input() title!: string
+  charArray: string[] = []
+
+  @ViewChild('titleCont', { static: true }) titleContRef!: ElementRef
+  randomInterval: number = 0
+
+  constructor(private renderer: Renderer2) {}
+
+  animRandomChar() {
+    const index: number = this.randomIntFromInterval(0,this.charArray.length + 1)
+    let randAnimDuration:number = this.randomIntFromInterval(1200, 2000);
     
-    @HostListener('document:mouseover', ['$event'])
-    mouseover(event: any) {
-        if (event.target.matches('.hidden-char')) {
-            const charIndex: number = event.target.classList[1]
-            
-            const element: any = document.getElementsByClassName('cw-' + charIndex)[0]
-            const charHeight: number = event.target.clientHeight
-            
-            element.style.transition = 'bottom 0.8s linear'
-            element.style.bottom = `-${charHeight}px`
-            
-            setTimeout(() => {
-                element.style.transition = 'none';
-                element.style.bottom = 0;
-                
-            }, 1000)
-        }
+    const charElement = this.titleContRef.nativeElement.querySelector('.cw-' + index)
+    if (charElement) {
+      const charHeight = charElement.offsetHeight / 2;
+      const transition = `bottom ${randAnimDuration}ms ease-in-out`;
+
+      this.renderer.setStyle(charElement, 'transition', transition)
+      this.renderer.setStyle(charElement, 'bottom', `-${charHeight}px`)
+
+      setTimeout(() => {
+        this.renderer.setStyle(charElement, 'transition', 'none')
+        this.renderer.setStyle(charElement, 'bottom', 0)
+      }, randAnimDuration)
     }
-    
-    randomIntFromInterval(min:number, max:number) {
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-    
-    ngOnInit(): void {
-        let wordsArray = this.title.split(' ')
-        let wa: string[] = []
-        wordsArray.forEach((w: string, index) => {
-            if (index !== wordsArray.length - 1) {
-                wa = [...wa, w, ' ']
-            } else {
-                wa = [...wa, w]
-            }
-        })
-        wordsArray = wa
-        wordsArray.forEach((w) => {
-            this.charArray = [...this.charArray, ...w.split('')]
-        })
-    }
-    
-    
-    ngAfterViewInit(): void {
-        
-        // const interval = setInterval(() => {
-        //     const randomIndex:number = this.randomIntFromInterval(0, this.charArray.length);    
-        // }, 1000)
-    }
-    
-    
-    
+  }
+
+  randomIntFromInterval(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  ngOnInit(): void {
+    let wordsArray = this.title.split(' ')
+    let wa: string[] = []
+    wordsArray.forEach((w: string, index) => {
+      if (index !== wordsArray.length - 1) {
+        wa = [...wa, w, ' ']
+      } else {
+        wa = [...wa, w]
+      }
+    })
+    wordsArray = wa
+    wordsArray.forEach((w) => {
+      this.charArray = [...this.charArray, ...w.split('')]
+    })
+  }
+
+  playAnim(){
+    setTimeout(() => {
+      this.randomInterval = this.randomIntFromInterval(1000, 5000)
+      this.animRandomChar();
+      this.playAnim();
+    }, this.randomInterval)
+  }
+  
+  ngAfterViewInit(): void {
+    this.randomInterval = this.randomIntFromInterval(1000, 5000)
+    this.playAnim();
+  }
 }
