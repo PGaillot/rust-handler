77020b14105c686c6a97f20b653124401e73014d
PGaillot <pr.gaillot@gmail.com>
2024-03-14 20:47:22 +0100

    fix messages disaply.

--- src/app/cmd-terminal/cmd-terminal.component.ts
+++ src/app/cmd-terminal/cmd-terminal.component.ts
@@ -9,10 +9,10 @@ import { Component, ElementRef, ViewChild } from '@angular/core'
     <div id="cmd-terminal">
       <div #cmdContent id="cmd-teminal-content">
         <span>
-        @for (message of messages.reverse(); track message) {
+          @for (message of messages.reverse(); track message) {
           <p>{{ message }}</p>
-        }
-      </span>
+          }
+        </span>
       </div>
       <div id="cmd-teminal-bottom">
         <p id="server-name">{{ serverName }}</p>
@@ -30,27 +30,44 @@ import { Component, ElementRef, ViewChild } from '@angular/core'
     </div>
   `,
 })
-export class CmdTerminalComponent{
+export class CmdTerminalComponent {
   @ViewChild('cmdInput', { static: true }) cmdInputRef!: ElementRef
   @ViewChild('cmdContent', { static: true }) cmdContentRef!: ElementRef
 
+  mgbMessage: string = `
+
+
+        ███╗   ███╗ ██████╗ ██████╗ 
+        ████╗ ████║██╔════╝ ██╔══██╗
+        ██╔████╔██║██║  ███╗██████╔╝
+        ██║╚██╔╝██║██║   ██║██╔══██╗
+        ██║ ╚═╝ ██║╚██████╔╝██████╔╝
+        ╚═╝     ╚═╝ ╚═════╝ ╚═════╝ 
+
+
+        - Gloire aux Mangeboules. -
+
+  `
+
   helpMessage: string = `
-  ┬ ┬┌─┐┬  ┌─┐
-  ├─┤├┤ │  ├─┘
-  ┴ ┴└─┘┴─┘┴  
-  
-  -------------------
-  NAVIGATION
-  - cd
-  | la commande de navigation sert à se deplacer de dossier 
-  | en dossier sur votre server. la commande [ cd ]
+
+
+    ░█░█░█▀▀░█░░░█▀█
+    ░█▀█░█▀▀░█░░░█▀▀
+    ░▀░▀░▀▀▀░▀▀▀░▀░░
+        
+    -------------------
+    NAVIGATION
+    - cd
+    | la commande de navigation sert à se deplacer de dossier 
+    | en dossier sur votre server. la commande [ cd ]
   
   `
 
   txtContent: string = ''
   messages: string[] = []
   serverName: string = 'user@server:'
-  triggerWords: string[] = ['rust', 'help']
+  triggerWords: string[] = ['rust', 'help', 'mgb']
 
   onEnterKeyPressed(event: Event) {
     const message: string = this.cmdInputRef.nativeElement.value.replace(
@@ -68,29 +85,36 @@ export class CmdTerminalComponent{
       this.cmdInputRef.nativeElement.disabled = true
       setTimeout(() => {
         switch (messageArray[0]) {
+
           case 'help':
             this.messages = [
               ...this.messages,
               this.serverName + this.helpMessage,
-            ];
-            break;
+            ]
+            break
+
           case 'rust':
-            this.messages = [...this.messages, 'rust message'];
-            break;
+            this.messages = [...this.messages, 'rust message']
+            break
+
+          case 'mgb':
+            this.messages = [
+              ...this.messages,
+              this.serverName + this.mgbMessage,
+            ]
+            break
 
           default:
-            this.messages = [...this.messages, 'default message'];
-            break;
+            this.messages = [...this.messages, 'default message']
+            break
         }
-        this.cmdInputRef.nativeElement.disabled = false;
-        this.cmdInputRef.nativeElement.focus();
-      }, 500);
+        this.cmdInputRef.nativeElement.disabled = false
+        this.cmdInputRef.nativeElement.focus()
+      }, 500)
     }
-    
+
     // Final
     console.log(this.messages)
-    ;
     this.cmdInputRef.nativeElement.value = '';
-    
   }
 }
